7c4eb487d2f864147ea037daac2ee03c99a3e2ef
Ігор Ломоносов <ipz224_lio@student.ztu.edu.ua>
2024-01-30 15:01:17 +0200

    Створено консольний проект для обрахування суми двох чисел

---
+++ Tech Prakt1/Tech Prakt1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TechPrakt
+{
+    class Program
+    {
+        static double Sum(double x, double y)
+        {
+            return x + y;
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Введіть перше число для обчислення:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введіть друге число для обчислення:");
+            double y = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Сума двох введених чисел: {Sum(x, y)}");
+        }
+    }
+}
